a8a30d5b1e662613a53a80f37e2a8af3ea115d97
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-07-21 07:30:32 +0200

    scripts: update script to convert .h2000 to .npy

--- scripts/h2000toarray.py
+++ scripts/h2000toarray.py
@@ -14,38 +14,42 @@ import ROOT
 side = 4
 mod = 64
 cell = 8
+cells = 48
 pulse = 7
+
+nFiles = 6
+
+eventLim = False
+numEvts = 1000
 
 # Leitura dos dados
 chain = ROOT.TChain("h2000","")
-filename = "tile_0_.aan.root"
+for i in range(1, nFiles+1):
+  filename = "data/user.pbragali.29676563.AANT._" + f"{i:06}" + ".root"
+  chain.Add(filename+"/h2000")
 
-chain.Add(filename+"/h2000")
+if eventLim:
+  if numEvts < chain.GetEntries():
+    nentries = numEvts
+  else:
+    nentries = chain.GetEntries()
+else:
+  nentries   = chain.GetEntries()
 
-nentries = chain.GetEntries()
+print("Total de eventos: ", nentries)
 
-print(nentries)
+sampleTMDB = []
+eOpt = []
 
-chain.GetEntry(0)
+for entryNum in range(0,nentries):
 
-run = getattr(chain,"Run")
-evt = getattr(chain,"Evt")
-evtNr = getattr(chain,"EvtNr")
+  chain.GetEntry(entryNum)
 
-aux_sample = getattr(chain,"sample")
-aux_sampleTMDB = getattr(chain,"sampleTMDB")
-aux_eOpt = getattr(chain,"eOpt")
+  aux_sampleTMDB = getattr(chain,"sampleTMDB")
+  aux_eOpt = getattr(chain,"eOpt")
 
-sample = np.array(aux_sample).reshape(side, mod, 48, pulse)
-sampleTMDB = np.array(aux_sampleTMDB).reshape(side, mod, cell, pulse)
-eOpt = np.array(aux_eOpt).reshape(side, mod, 48)
+  sampleTMDB.append(np.array(aux_sampleTMDB).reshape(side, mod, cell, pulse))
+  eOpt.append(np.array(aux_eOpt).reshape(side, mod, cells))
 
-
-for i in range(0, mod):
-  print("\n", i)
-  print(eOpt[0,i,0])
-  print(sample[0,i,0,:])
-  print(sampleTMDB[0,i,0,:])
-
-np.save('sampleTMDB.npy',sampleTMDB)
-np.save('eOpt.npy',eOpt)
+np.save('sampleTMDB.npy', sampleTMDB)
+np.save('eOpt.npy', eOpt)
